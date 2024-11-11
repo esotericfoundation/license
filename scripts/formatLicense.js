f333610abd2760a5de2954ec600c906a057a3f6b
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-11 22:49:46 +0000

    Format the correct file

--- scripts/formatLicense.js
+++ scripts/formatLicense.js
@@ -1,9 +1,9 @@
 const { readFileSync, writeFileSync } = require("fs")
 const { wrapText } = require("esoteric-utility");
 
-const licenseText = readFileSync("LICENSE.txt").toString();
+const licenseText = readFileSync("Esoteric Foundation Public License.txt").toString();
 const maxLength = 71;
 
 const wrappedLicense = wrapText(licenseText, maxLength, /(?<=^\s*)((\d+)|([a-z]))\. /);
 
-writeFileSync("LICENSE.txt", wrappedLicense);
+writeFileSync("Esoteric Foundation Public License.txt", wrappedLicense);
