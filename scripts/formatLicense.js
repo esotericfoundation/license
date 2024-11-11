1a734bfd1d4176c9cb3e0f381034dffe59b60039
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-11 22:48:57 +0000

    Update license formatting script

--- scripts/formatLicense.js
+++ scripts/formatLicense.js
@@ -2,8 +2,8 @@ const { readFileSync, writeFileSync } = require("fs")
 const { wrapText } = require("esoteric-utility");
 
 const licenseText = readFileSync("LICENSE.txt").toString();
-const maxLength = 72;
+const maxLength = 71;
 
-const wrappedLicense = wrapText(licenseText, maxLength);
+const wrappedLicense = wrapText(licenseText, maxLength, /(?<=^\s*)((\d+)|([a-z]))\. /);
 
 writeFileSync("LICENSE.txt", wrappedLicense);
