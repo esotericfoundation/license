49c2d7e9045c2bdd92fd6cdf4081f7cfb772de5a
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-11 22:52:22 +0000

    Detect roman numerals with the RegExp

--- scripts/formatLicense.js
+++ scripts/formatLicense.js
@@ -4,6 +4,6 @@ const { wrapText } = require("esoteric-utility");
 const licenseText = readFileSync("Esoteric Foundation Public License.txt").toString();
 const maxLength = 71;
 
-const wrappedLicense = wrapText(licenseText, maxLength, /(?<=^\s*)((\d+)|([a-z]))\. /);
+const wrappedLicense = wrapText(licenseText, maxLength, /(?<=^\s*)((\d+)|([a-z])|m{0,4}(cm|cd|d?c{0,3})(xc|xl|l?x{0,3})(ix|iv|v?i{0,3}))\. /);
 
 writeFileSync("Esoteric Foundation Public License.txt", wrappedLicense);
