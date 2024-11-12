e36fca197ffa40a1b5e85df9b9e1a882f7ea65ba
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-12 17:12:58 +0000

    Format the scripts

--- scripts/formatLicense.js
+++ scripts/formatLicense.js
@@ -1,4 +1,4 @@
-const { readFileSync, writeFileSync } = require("fs")
+const { readFileSync, writeFileSync } = require("fs");
 const { wrapText } = require("esoteric-utility");
 
 const licenseText = readFileSync("Esoteric Foundation Public License.txt").toString();
